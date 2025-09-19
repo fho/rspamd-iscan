2743ed10a87596edb8a182d647685953228a0861
Fabian Holler <mail@fholler.de>
2025-09-19 11:24:04 +0200

    fix: close is called for former iscan.Clients instances on retry
    When a retryable error occurs in main(), the signal handler that calls
    close on iscan.Client is kept installed.
    
    This means the iscan.Client instances must also be kept in memory. This
    is a memory leak.
    
    It is fixed by refactoring main() and removing all installed signal
    handlers when a client returns an error, before a new one is created.
    
    It would be good to split creating a Client and establishing the
    connection into 2 methods in a follow-up, to prevent recreating the
    iscan.Client unnecessarily on every retry.

--- main.go
+++ main.go
@@ -42,9 +42,31 @@ func parseFlags() *flags {
 	return &result
 }
 
+func configureLogger() *slog.Logger {
+	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+			// do not log timestamp, rspamd-iscan is normally run as
+			// daemon, journald/syslog already adds timestamps
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+	})
+
+	return slog.New(h)
+}
+
+var handledSignals = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+
+func removeSigHandler() {
+	signal.Reset(handledSignals...)
+}
+
 func installSigHandler(logger *slog.Logger, clt *iscan.Client) {
 	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(sigCh, handledSignals...)
 
 	go func() {
 		var strSig string
@@ -63,6 +85,97 @@ func installSigHandler(logger *slog.Logger, clt *iscan.Client) {
 	}()
 }
 
+func newIscanClient(
+	cfg *config.Config,
+	logger *slog.Logger,
+	rspamc iscan.RspamdClient,
+) (*iscan.Client, error) {
+	clt, err := iscan.NewClient(&iscan.Config{
+		ServerAddr:            cfg.ImapAddr,
+		User:                  cfg.ImapUser,
+		Password:              cfg.ImapPassword,
+		ScanMailbox:           cfg.ScanMailbox,
+		InboxMailbox:          cfg.InboxMailbox,
+		HamMailbox:            cfg.HamMailbox,
+		SpamMailboxName:       cfg.SpamMailbox,
+		UndetectedMailboxName: cfg.UndetectedMailbox,
+		BackupMailbox:         cfg.BackupMailbox,
+		SpamTreshold:          cfg.SpamThreshold,
+		TempDir:               cfg.TempDir,
+		KeepTempFiles:         cfg.KeepTempFiles,
+		Logger:                logger,
+		Rspamc:                rspamc,
+	})
+	if err != nil {
+		logger.Error("creating iscan client failed", "error", err)
+	}
+
+	return clt, err
+}
+
+func runOnceAndTerminate(
+	cfg *config.Config,
+	logger *slog.Logger,
+	rspamc iscan.RspamdClient,
+) {
+	clt, err := newIscanClient(cfg, logger, rspamc)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := clt.RunOnce(); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func mustMonitorUntilFatalError(
+	cfg *config.Config,
+	logger *slog.Logger,
+	rspamc iscan.RspamdClient,
+) {
+	for {
+		err := monitor(cfg, logger, rspamc)
+		if err != nil {
+			rError := &iscan.ErrRetryable{}
+			if !errors.As(err, &rError) {
+				logger.Error("non-retryable error occurred, terminating", "error", err)
+				os.Exit(1)
+			}
+
+			logger.Error("retryable error occurred, restarting iscan monitoring process", "error", err)
+			continue
+		}
+
+		logger.Info("iscan process terminated normally, shutting down")
+		os.Exit(0)
+	}
+}
+
+func monitor(
+	cfg *config.Config,
+	logger *slog.Logger,
+	rspamc iscan.RspamdClient,
+) error {
+	clt, err := newIscanClient(cfg, logger, rspamc)
+	if err != nil {
+		return err
+	}
+
+	installSigHandler(logger, clt)
+	defer removeSigHandler()
+
+	err = clt.Monitor()
+	if err != nil {
+		_ = clt.Stop()
+		return fmt.Errorf("monitoring imap mailboxes failed: %w", err)
+	}
+
+	_ = clt.Stop()
+	return nil
+}
+
 func main() {
 	flags := parseFlags()
 	if flags.printVersion {
@@ -70,18 +183,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
-			// do not log timestamp, rspamd-iscan is normally run as
-			// daemon, journald/syslog already adds timestamps
-			if a.Key == slog.TimeKey {
-				return slog.Attr{}
-			}
-			return a
-		},
-	})
-	logger := slog.New(h)
+	logger := configureLogger()
 
 	cfg, err := config.FromFile(flags.cfgPath)
 	if err != nil {
@@ -96,52 +198,9 @@ func main() {
 	// TODO: allow passing all attrs as single URL to rspamc http client
 	rspamc := rspamc.New(logger, cfg.RspamdURL, cfg.RspamdPassword)
 
-	for {
-		clt, err := iscan.NewClient(&iscan.Config{
-			ServerAddr:            cfg.ImapAddr,
-			User:                  cfg.ImapUser,
-			Password:              cfg.ImapPassword,
-			ScanMailbox:           cfg.ScanMailbox,
-			InboxMailbox:          cfg.InboxMailbox,
-			HamMailbox:            cfg.HamMailbox,
-			SpamMailboxName:       cfg.SpamMailbox,
-			UndetectedMailboxName: cfg.UndetectedMailbox,
-			BackupMailbox:         cfg.BackupMailbox,
-			SpamTreshold:          cfg.SpamThreshold,
-			TempDir:               cfg.TempDir,
-			KeepTempFiles:         cfg.KeepTempFiles,
-			Logger:                logger,
-			Rspamc:                rspamc,
-		})
-		if err != nil {
-			logger.Error("creating imap client failed", "error", err)
-			os.Exit(1)
-		}
-
-		installSigHandler(logger, clt)
-
-		if flags.once {
-			if err := clt.RunOnce(); err != nil {
-				logger.Error(err.Error())
-				os.Exit(1)
-			}
-			os.Exit(0)
-		}
-
-		err = clt.Monitor()
-		if err != nil {
-			_ = clt.Stop()
-			rError := &iscan.ErrRetryable{}
-			if !errors.As(err, &rError) {
-				logger.Error("non-retryable error occurred, terminating", "error", err)
-				os.Exit(1)
-			}
-
-			logger.Error("retryable error occurred, restarting monitoring iscan process", "error", err)
-			continue
-		}
-		_ = clt.Stop()
-		logger.Info("iscan process terminated normally, shutting down")
-		return
+	if flags.once {
+		runOnceAndTerminate(cfg, logger, rspamc)
+	} else {
+		mustMonitorUntilFatalError(cfg, logger, rspamc)
 	}
 }
