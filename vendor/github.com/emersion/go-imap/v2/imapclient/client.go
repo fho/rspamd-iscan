bfebc68625bfed3e55c603b45844089b0d45b6ff
Fabian Holler <mail@fholler.de>
2025-12-14 22:32:23 +0100

    go: update go-imap/v2 to v2.0.0-beta.7

--- vendor/github.com/emersion/go-imap/v2/imapclient/client.go
+++ vendor/github.com/emersion/go-imap/v2/imapclient/client.go
@@ -113,7 +113,7 @@ func (options *Options) unilateralDataHandler() *UnilateralDataHandler {
 }
 
 func (options *Options) tlsConfig() *tls.Config {
-	if options != nil && options.TLSConfig != nil {
+	if options.TLSConfig != nil {
 		return options.TLSConfig.Clone()
 	} else {
 		return new(tls.Config)
@@ -220,6 +220,10 @@ func NewStartTLS(conn net.Conn, options *Options) (*Client, error) {
 
 // DialInsecure connects to an IMAP server without any encryption at all.
 func DialInsecure(address string, options *Options) (*Client, error) {
+	if options == nil {
+		options = &Options{}
+	}
+
 	conn, err := options.dialer().Dial("tcp", address)
 	if err != nil {
 		return nil, err
@@ -229,6 +233,10 @@ func DialInsecure(address string, options *Options) (*Client, error) {
 
 // DialTLS connects to an IMAP server with implicit TLS.
 func DialTLS(address string, options *Options) (*Client, error) {
+	if options == nil {
+		options = &Options{}
+	}
+
 	tlsConfig := options.tlsConfig()
 	if tlsConfig.NextProtos == nil {
 		tlsConfig.NextProtos = []string{"imap"}
