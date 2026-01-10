fho/rspamd-iscan

commit 73a93020e7fe106e07d14e39b27aeee22bd5f7a8

iscan: generalize iscan.ErrRetryable, retry on imap connection errors
iscan.ErrRetryable is replaced by a neterr.IsRetryableError function.
It checks if an error is a network error that might go away when
retrying.

This alllows to also evaluate if errors from any function are retryable.
It also simplifies the code, by not having to wrap errors when returning
them anymore. Instead we can pass the function to the retry.Runner.

In practice, rspamd-iscan will now also retry establishing a connection
to an imap server if it is likely a temporarily network error.

internal/neterr/error.go
@@ -1,0 +1,23 @@
+package neterr
+
+import (
+	"errors"
+	"io"
+	"syscall"
+)
+
+func IsRetryableError(err error) bool {
+	switch {
+	case errors.Is(err, syscall.ECONNREFUSED),
+		errors.Is(err, syscall.ECONNRESET),
+		errors.Is(err, syscall.ECONNABORTED),
+		errors.Is(err, syscall.EPIPE),
+		errors.Is(err, syscall.ETIMEDOUT),
+		errors.Is(err, syscall.ENETUNREACH),
+		errors.Is(err, syscall.EHOSTUNREACH),
+		errors.Is(err, io.ErrUnexpectedEOF):
+		return true
+	default:
+		return false
+	}
+}

internal/neterr/error_test.go
@@ -1,0 +1,15 @@
+package neterr
+
+import (
+	"net"
+	"testing"
+
+	"github.com/fho/rspamd-iscan/internal/testutils/assert"
+)
+
+func TestIsRetryableError_ConnectionRefused(t *testing.T) {
+	_, err := net.Dial("tcp", "localhost:59123") // port where nothing is listening
+	t.Logf("error: %v", err)
+	assert.Error(t, err)
+	assert.Equal(t, true, IsRetryableError(err))
+}

internal/iscan/error.go
@@ -1,41 +1,0 @@
-package iscan
-
-import (
-	"errors"
-	"log/slog"
-	"net"
-	"strings"
-)
-
-type ErrRetryable struct {
-	err error
-}
-
-func (e *ErrRetryable) Error() string {
-	return "ErrRetryable: " + e.err.Error()
-}
-
-func (e *ErrRetryable) UnwrapError() error {
-	return e
-}
-
-func WrapRetryableError(err error) error {
-	var ne *net.OpError
-
-	if strings.Contains(err.Error(), "use of closed network connection") {
-		return &ErrRetryable{err: err}
-	}
-
-	if strings.Contains(err.Error(), "read: permission denied") {
-		return &ErrRetryable{err: err}
-	}
-
-	if errors.As(err, &ne) {
-		if ne.Temporary() {
-			return &ErrRetryable{err: err}
-		}
-		slog.Debug("errRetryable: not a temporary error: ", "error", ne.Error())
-	}
-
-	return err
-}

internal/iscan/client.go
@@ -447,7 +447,7 @@ func (c *Client) Monitor() error {
 	defer c.wgRun.Done()
 
 	if err := c.RunOnce(); err != nil {
-		return WrapRetryableError(err)
+		return err
 	}
 
 	lastLearnAt := time.Now()
@@ -455,7 +455,7 @@ func (c *Client) Monitor() error {
 	for {
 		eventCh, monitorCancelFn, err := c.clt.Monitor(c.scanMailbox)
 		if err != nil {
-			return WrapRetryableError(err)
+			return err
 		}
 
 		c.logger.Debug("waiting for mailbox update events")
@@ -464,7 +464,7 @@ func (c *Client) Monitor() error {
 			c.logger.Debug("learn timer expired, checking mailboxes for new messages")
 
 			if err := monitorCancelFn(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			// sometimes monitoring stopped working and no updates
@@ -473,15 +473,15 @@ func (c *Client) Monitor() error {
 			// TODO: verify if that really is still an issue or
 			// could be removed
 			if err := c.ProcessScanBox(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			if err := c.ProcessHam(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			if err := c.ProcessSpam(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			lastLearnAt = time.Now()
@@ -494,7 +494,7 @@ func (c *Client) Monitor() error {
 			}
 
 			if err := monitorCancelFn(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			if evA.NewMsgCount == 0 {
@@ -504,12 +504,12 @@ func (c *Client) Monitor() error {
 
 			err = c.ProcessScanBox()
 			if err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 		case <-c.stopCh:
 			if err := monitorCancelFn(); err != nil {
-				return WrapRetryableError(err)
+				return err
 			}
 
 			return nil
@@ -521,12 +521,12 @@ func (c *Client) Monitor() error {
 func (c *Client) RunOnce() error {
 	err := c.ProcessHam()
 	if err != nil {
-		return fmt.Errorf("learning ham failed: %w", WrapRetryableError(err))
+		return fmt.Errorf("learning ham failed: %w", err)
 	}
 
 	err = c.ProcessSpam()
 	if err != nil {
-		return fmt.Errorf("learning spam failed: %w", WrapRetryableError(err))
+		return fmt.Errorf("learning spam failed: %w", err)
 	}
 
 	return c.ProcessScanBox()

main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log/slog"
 	"os"
@@ -12,6 +11,7 @@ import (
 	"github.com/fho/rspamd-iscan/internal/config"
 	"github.com/fho/rspamd-iscan/internal/imapclt"
 	"github.com/fho/rspamd-iscan/internal/iscan"
+	"github.com/fho/rspamd-iscan/internal/neterr"
 	"github.com/fho/rspamd-iscan/internal/retry"
 	"github.com/fho/rspamd-iscan/internal/rspamc"
 
@@ -221,11 +221,8 @@ func run() error {
 	fmt.Printf("Monitoring IMAP mailboxes continuously.\n\n")
 
 	retryRunner := retry.Runner{
-		Fn: func() error { return monitor(cfg, flags, logger, rspamc) },
-		IsRetryable: func(err error) bool {
-			rError := &iscan.ErrRetryable{}
-			return errors.As(err, &rError)
-		},
+		Fn:                  func() error { return monitor(cfg, flags, logger, rspamc) },
+		IsRetryable:         neterr.IsRetryableError,
 		MaxRetriesSameError: 10,
 		RetryIntervals: []time.Duration{
 			3 * time.Second,

commit b65cd007995c090f277720f7c14d4162639abd77

fix: retry monitoring on closed network connection again
73a9302 introduced the issue that rspamd-iscan is not restarting the
monitor loop anymore when a connection was closed.

Retry when a net.ErrClosed happens.

internal/neterr/error.go
@@ -3,6 +3,7 @@ package neterr
 import (
 	"errors"
 	"io"
+	"net"
 	"syscall"
 )
 
@@ -15,7 +16,8 @@ func IsRetryableError(err error) bool {
 		errors.Is(err, syscall.ETIMEDOUT),
 		errors.Is(err, syscall.ENETUNREACH),
 		errors.Is(err, syscall.EHOSTUNREACH),
-		errors.Is(err, io.ErrUnexpectedEOF):
+		errors.Is(err, io.ErrUnexpectedEOF),
+		errors.Is(err, net.ErrClosed):
 		return true
 	default:
 		return false

internal/neterr/error_test.go
@@ -13,3 +13,19 @@ func TestIsRetryableError_ConnectionRefused(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, true, IsRetryableError(err))
 }
+
+func TestIsRetryableError_ClosedConnection(t *testing.T) {
+	ln, err := net.Listen("tcp", "localhost:0")
+	assert.NoError(t, err)
+	defer ln.Close()
+
+	conn, err := net.Dial("tcp", ln.Addr().String())
+	assert.NoError(t, err)
+
+	conn.Close()
+
+	_, err = conn.Write([]byte("test"))
+	t.Logf("error: %v", err)
+	assert.Error(t, err)
+	assert.Equal(t, true, IsRetryableError(err))
+}